9a7788e7da266cd71c0864abe01446f4033fe27f
nickVerve <nick@vervesearch.com>
2021-10-15 14:48:06 +0100

    Update Dialog context to allow props to be passed in to setDialogFunc, simplify dialog props and css

--- app/templates/src/components/ui/Dialog/Dialog.tsx
+++ app/templates/src/components/ui/Dialog/Dialog.tsx
@@ -8,12 +8,6 @@ export interface DialogProps {
     unSetDialog: () => void;
     close?: () => void;
     className?: string;
-    position?: 'top' | 'bottom' | 'left' | 'right' | 'middle';
-    spacing?: 'small' | 'medium' | 'large';
-    wrapHeight?: 'default' | 'full-height' | 'small-height' | 'medium-height' | 'large-height';
-    wrapWidth?: 'default' | 'full-width' | 'small-width' | 'medium-width' | 'large-width';
-    backdropColor?: string;
-    dialogColor?: string;
     animateIn?: string;
     animateOut?: string;
 }
@@ -24,22 +18,11 @@ const Dialog: React.FC<DialogProps> = ({
     dialog,
     unSetDialog,
     className = '',
-    position = 'middle',
     close = () => ({}),
-    spacing = 'medium',
-    wrapHeight = 'default',
-    wrapWidth = 'default',
-    backdropColor = null,
-    dialogColor = null,
     animateIn = 'fadeIn',
     animateOut = 'fadeOut',
-    children,
 }) => {
     const clz = className;
-    const pos = 'dialog--' + position;
-    const spacingCls = 'dialog--spacing-' + spacing;
-    const wrapHeightCls = 'dialog--' + wrapHeight;
-    const wrapWidthCls = 'dialog--' + wrapWidth;
 
     const backdropEl = React.useRef(null);
     const contentWrapperEl = React.useRef(null);
@@ -55,22 +38,16 @@ const Dialog: React.FC<DialogProps> = ({
     useLockBodyScroll();
 
     return ReactDOM.createPortal(
-        <div className={`dialog ${clz} ${pos} ${spacingCls} ${wrapHeightCls} ${wrapWidthCls}`}>
+        <div className={`dialog ${clz}`}>
             <div
                 className="dialog__backdrop animated speed-5 fadeIn"
                 onClick={close}
                 id="DialogBackdrop"
-                style={{
-                    backgroundColor: backdropColor,
-                }}
                 ref={backdropEl}
             ></div>
 
             <div
                 className={`dialog__wrapper animated speed-5 ${animateIn}`}
-                style={{
-                    backgroundColor: dialogColor,
-                }}
                 ref={contentWrapperEl}
             >
                 <div
--- app/templates/src/hooks.tsx
+++ app/templates/src/hooks.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import Dialog from './components/ui/Dialog/Dialog';
+
+import Dialog, { DialogProps } from './components/ui/Dialog/Dialog';
 
 const { useLayoutEffect } = React;
 
@@ -20,18 +21,33 @@ export const useLockBodyScroll = () => {
 /**
  * Allows components to use a Dialog/Modal
  */
-const DialogContext = React.createContext(null);
+interface DialogWithProps {
+    dialogComponent: JSX.Element | JSX.Element[];
+    dialogProps: Partial<DialogProps>;
+}
+
+interface DialogContext {
+    setDialog: (dialogComponent: JSX.Element | JSX.Element[], dialogProps?: Partial<DialogProps>) => void;
+    unSetDialog: () => void;
+    dialog: DialogWithProps;
+}
+
+const DialogContext = React.createContext<DialogContext | null>(null);
 
 export const DialogProvider = (props) => {
-    const [dialog, setDialog] = React.useState();
+    const [dialog, setDialogState] = React.useState<DialogWithProps | null>();
+
+    const setDialog = (dialogComponent: JSX.Element | JSX.Element[], dialogProps: Partial<DialogProps> = null) => {
+        setDialogState({ dialogComponent, dialogProps });
+    };
     const unSetDialog = React.useCallback(() => {
-        setDialog(null);
-    }, [setDialog]);
+        setDialogState(null);
+    }, [setDialogState]);
 
     return (
         <DialogContext.Provider value={{ unSetDialog, setDialog, dialog }} {...props}>
             {props.children}
-            {dialog && <Dialog dialog={dialog} unSetDialog={unSetDialog} />}
+            {dialog && <Dialog dialog={dialog.dialogComponent} unSetDialog={unSetDialog} {...dialog.dialogProps} />}
         </DialogContext.Provider>
     );
 };
