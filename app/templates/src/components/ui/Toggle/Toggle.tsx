1998858b772d6be4f6ac6aed055def655fb7ceec
nickVerve <nick@vervesearch.com>
2021-10-19 11:38:23 +0100

    Add Toggle component

---
+++ app/templates/src/components/ui/Toggle/Toggle.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+export interface ToggleProps {
+    className?: string;
+    labels: string[];
+    toggleIdx: number;
+    setToggleIdx: (idx: number) => void;
+}
+
+const Toggle: React.FC<ToggleProps> = ({ className, labels, toggleIdx, setToggleIdx }) => {
+    const cls = className || '';
+
+    return (
+        <div className={'toggle ' + cls}>
+            {labels.map((label, idx) => (
+                <div
+                    key={label}
+                    className={`toggle__item ${toggleIdx === idx ? 'toggle__item--active' : ''}`}
+                    onClick={() => setToggleIdx(idx)}
+                    onKeyDown={(e) => (e.key === 'Enter' ? setToggleIdx(idx) : null)}
+                    aria-label={label}
+                    role="button"
+                    tabIndex={0}
+                >
+                    {label}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Toggle;
